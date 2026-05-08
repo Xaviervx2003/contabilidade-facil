fe5d60d1102ad6fff5e04ded43fc5c5cdeabeae5
Xaviervx2003 <ALOK.DJ.GOD@GMAIL.COM>
2026-05-08 15:26:52 -0400

    fix: instalação física do tailwind e failsafe de estilos premium

--- painel-admin/src/index.jsx
+++ painel-admin/src/index.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import 'core-js'
 
-import './tailwind.css'
 import App from './App'
 import store from './store'
 
